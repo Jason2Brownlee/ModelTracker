fab19c81b376b7c70c3069f7b54dba1672a45902
Jason Brownlee <jason.brownlee05@gmail.com>
2024-08-26 06:42:32 +1000

    improved docs for custom functions

--- src/custom_config.py
+++ src/custom_config.py
@@ -13,16 +13,28 @@ def load_dataset():
     X, y = data.data, data.target
     return X, y
 
+# Example: Load a custom CSV file
+"""
+def load_dataset():
+    import pandas as pd
+    data = pd.read_csv("path/to/your/dataset.csv")
+    X = data.drop("target_column", axis=1).values
+    y = data["target_column"].values
+    return X, y
+"""
+
 def default_resampling_method(X, y):
     """
     Default resampling method: cross-validation with 5 folds.
     This function can be customized to use different resampling methods.
+    See https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.cross_val_score.html
     """
-    return cross_val_score, {'cv': 5}
+    return cross_val_score, {'cv': 5,}
 
 def default_evaluation_metric():
     """
     Default evaluation metric: accuracy score.
     This function can be customized to use different evaluation metrics.
+    See https://scikit-learn.org/stable/modules/model_evaluation.html
     """
     return 'accuracy'
